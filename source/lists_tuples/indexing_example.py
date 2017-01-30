9ab82698019bfbf406d9cae0db072870b19276ec
Gavin <tianyu_0723@hotmail.com>
2017-01-30 21:03:26 +0800

    序列示例

--- source/lists_tuples/indexing_example.py
+++ source/lists_tuples/indexing_example.py
@@ -1,7 +1,36 @@
-# 字符串就是由字符组成的序列
-greeting = 'Hello'
-print('索引0第一个元素：' + greeting[0])
-print('索引-1最后一个元素：' + greeting[-1])
+"""
+序列示例
+"""
 
+# 1-12月的序列
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
 
+# 以 1-31 的数字作为结尾的列表
+endings = ['st','nd','rd'] + 17 * ['th'] + ['st','nd','rd'] + 7 * ['th'] + ['st']
+
+year = input('Year: ')
+month = input('Month（1-12）：')
+day = input('Day（1-31）：')
+
+month_number = int(month)
+day_number = int(day)
+
+# 月份与天数减一，获取正确索引
+month_name = months[month_number - 1]
+ordinal = day + endings[day_number - 1]
+
+print(month_name + ' ' + ordinal + '，' + year)
 
