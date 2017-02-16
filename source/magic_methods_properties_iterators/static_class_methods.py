deb267981a38f486ae177024542c33ff44f6b417
Gavin <tianyu_0723@hotmail.com>
2017-02-16 18:15:41 +0800

    静态方法和类成员方法

---
+++ source/magic_methods_properties_iterators/static_class_methods.py
@@ -0,0 +1,13 @@
+class MyClass:
+
+    @staticmethod
+    def smeth():
+        print('This is a static method')
+
+    @classmethod
+    def cmeth(cls):
+        print('This is a class method of', cls)
+
+
+MyClass.smeth()
+MyClass.cmeth()
