c5c0ad7a0d2de26961e2e34f21d70ac405a52651
Gavin <tianyu_0723@hotmail.com>
2017-02-16 18:15:15 +0800

    迭代器

---
+++ source/magic_methods_properties_iterators/iterators.py
@@ -0,0 +1,37 @@
+# 迭代器规则
+class Fibs:
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        return self.a
+
+    def __iter__(self):
+        return self
+
+
+fibs = Fibs()
+for f in fibs:
+    if f > 1000:
+        print(f)
+        break
+
+
+# 从迭代器得到序列
+class TestIterator:
+    value = 0
+
+    def __next__(self):
+        self.value += 1
+        if self.value > 10:
+            raise StopIteration
+        return self.value
+
+    def __iter__(self):
+        return self
+
+
+ti = TestIterator()
+print(list(ti))
