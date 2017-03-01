532aa6282c688e3ce452a8d71759f4097bb8dd62
Gavin <tianyu_0723@hotmail.com>
2017-03-01 15:39:29 +0800

    数据库查询

---
+++ source/database/food_query.py
@@ -0,0 +1,15 @@
+import sqlite3,sys
+
+conn = sqlite3.connect('food.db')
+curs = conn.cursor()
+
+query = 'select * from food where %s' % sys.argv[1]
+
+print(query)
+
+curs.execute(query)
+names = [f[0] for f in curs.description]
+
+for row in curs.fetchall():
+    for pair in zip(names, row):
+        print('%s:%s' % pair)
