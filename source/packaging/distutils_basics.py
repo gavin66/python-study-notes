c61e82d41624703b08b0966178c8171757f07d13
Gavin <tianyu_0723@hotmail.com>
2017-03-06 17:47:22 +0800

    Distutils 安装打包

---
+++ source/packaging/distutils_basics.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+import sys
+sys.path.append('/Users/Gavin/PycharmProjects/python-study-notes/source/packaging')
+
+setup(name='Hello', version='1.0', description='A simple example', author='Gavin', py_modules=['hello'])
